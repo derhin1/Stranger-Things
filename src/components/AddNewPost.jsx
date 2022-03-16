cc27b139f68492c638abfbda3257a5d043659625
Alejandro <alexsagarnaga@gmail.com>
2022-03-16 12:49:49 -0500

    upon clicking submit at add new post, fixed redirecting to posts page updating it with our new post

--- src/components/AddNewPost.jsx
+++ src/components/AddNewPost.jsx
@@ -1,76 +1,76 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { postNewPost } from "../api";
 import { useHistory } from "react-router-dom";
 
-const AddNewPost = ({loginState}) => {
-    const history = useHistory();
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    const [location, setLocation] = useState('')
-    const [deliver, setDeliver] = useState(false);
+const AddNewPost = ({ loginState }) => {
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [location, setLocation] = useState("");
+  const [deliver, setDeliver] = useState(false);
 
-    async function checkLoggedIn(title,description,price,location,deliver){
-        if(loginState){
-             await postNewPost(title, description, price, location, deliver);
-        }
-        else{
-           history.push('/Login')
-        }
+  async function checkLoggedIn(title, description, price, location, deliver) {
+    if (loginState) {
+      await postNewPost(title, description, price, location, deliver);
+      history.push("/Posts");
+    } else {
+      history.push("/Login");
     }
+  }
 
-    return (
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          checkLoggedIn(title,description,price,location,deliver)
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        checkLoggedIn(title, description, price, location, deliver);
+      }}
+    >
+      <input
+        type="Text"
+        placeholder="Title"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
         }}
-      >
+      ></input>
+      <input
+        type="Text"
+        placeholder="Description"
+        value={description}
+        onChange={(event) => {
+          setDescription(event.target.value);
+        }}
+      ></input>
+      <input
+        type="Text"
+        placeholder="Price"
+        value={price}
+        onChange={(event) => {
+          setPrice(event.target.value);
+        }}
+      ></input>
+      <input
+        type="Text"
+        placeholder="Location"
+        value={location}
+        onChange={(event) => {
+          setLocation(event.target.value);
+        }}
+      ></input>
+      <>
         <input
-          type="Text"
-          placeholder="Title"
-          value={title}
-          onChange={(event) => {
-            setTitle(event.target.value);
+          type="checkbox"
+          onChange={() => {
+            let nextValue = !deliver;
+            setDeliver(nextValue);
           }}
         ></input>
-        <input
-          type="Text"
-          placeholder="Description"
-          value={description}
-          onChange={(event) => {
-            setDescription(event.target.value);
-          }}
-        ></input>
-        <input
-          type="Text"
-          placeholder="Price"
-          value={price}
-          onChange={(event) => {
-            setPrice(event.target.value);
-          }}
-        ></input>
-        <input
-          type="Text"
-          placeholder="Location"
-          value={location}
-          onChange={(event) => {
-            setLocation(event.target.value);
-          }}
-        ></input>
-        <>
-          <input
-            type="checkbox"
-            onChange={() => {
-              let nextValue = !deliver;
-              setDeliver(nextValue);
-            }}
-          ></input>
-          <span style={{ marginLeft: "6px" }}>Will Deliver?</span>
-        </>
-        <button type="submit"> Submit </button>
-      </form>
-    );
-}
+        <span style={{ marginLeft: "6px" }}>Will Deliver?</span>
+      </>
+      <button type="submit"> Submit </button>
+    </form>
+  );
+};
 
-export default AddNewPost
+export default AddNewPost;
