bc4561251248b5bbdb5241f37a01e6f873a07d9b
Derrick Hinh <derhin1@gmail.com>
2022-03-17 15:37:17 -0500

    wrapped the entire return in profile into a turnary, and placed all the html into functions for cleanliness

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,30 +3,47 @@ import React from 'react'
 const Profile = ({userObj}) => {
     console.log(userObj, 'userObj')
 
-    return (
-      <>
-        <div>Welcome {`${userObj.username}`}</div>
-
-        <h3>Message Inbox</h3>
-        {userObj.messages.map((element) => {
-          return userObj.username != element.fromUser.username ? (
-            <div>
+    function inboxDisplay(element){
+        return (
+          <div>
             <div>From: {`${element.fromUser.username}`}</div>
             <div>{`${element.content}`}</div>
             <div>Post: {`${element.post.title}`}</div>
-            </div>
-          ) : null;
+          </div>
+        );
+    }
+
+    function myMessagesDisplay(element){
+        return (
+          <div>
+            <div>Sent By Me</div>
+            <div>{`${element.content}`}</div>
+          </div>
+        );
+    }
+
+
+    function messages(userObj){
+        return (
+            <>
+            <div>Welcome {`${userObj.username}`}</div>
+
+        <h3>Message Inbox</h3>
+        {userObj.messages.map((element) => {
+          return userObj.username != element.fromUser.username ? inboxDisplay(element) : null;
         })}
 
         <h3>Messages Sent</h3>
         {userObj.messages.map((element) => {
-          return userObj.username === element.fromUser.username ? (
-            <div>
-                <div>Sent By Me</div>
-              <div>{`${element.content}`}</div>
-            </div>
-          ) : null;
+          return userObj.username === element.fromUser.username ? myMessagesDisplay(element) : null;
         })}
+        </>
+        )
+    }
+
+    return ( 
+      <>
+      {userObj.username != null ? messages(userObj) : null}
       </>
     );
 }
