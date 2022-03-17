1c0059078b31134b66c2af5bbcad2aa83855c8d8
Derrick Hinh <derhin1@gmail.com>
2022-03-17 15:50:42 -0500

    From profile, the messages now display and link to the relevant post when clicked

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
-
+import { useHistory } from "react-router-dom";
 const Profile = ({userObj}) => {
-    console.log(userObj, 'userObj')
+    // console.log(userObj, 'userObj')
+    const history = useHistory();
 
     function inboxDisplay(element){
         return (
           <div>
             <div>From: {`${element.fromUser.username}`}</div>
             <div>{`${element.content}`}</div>
-            <div>Post: {`${element.post.title}`}</div>
+            <a onClick={() => {
+                handleClick(element.post._id)
+            }}>View My Post: {`${element.post.title}`}</a>
           </div>
         );
+    }
+
+    function handleClick(postId){
+        history.push(`/Posts/${postId}`)
     }
 
     function myMessagesDisplay(element){
         return (
           <div>
-            <div>Sent By Me</div>
+            <div>(Sent By Me)</div>
             <div>{`${element.content}`}</div>
+            <a
+              onClick={() => {
+                handleClick(element.post._id);
+              }}
+            >
+              Message Again: {`${element.post.title}`}
+            </a>
           </div>
         );
     }
