bcac3187cbbc1b6ba6f0c8eff330743c3043d3e0
Derrick Hinh <derhin1@gmail.com>
2022-03-17 18:45:35 -0500

    Fixed the home page white screen error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,8 @@ const Home = ({ loginState, userObj }) => {
     <>
       <h1> Welcome to Stranger's Things!</h1>
       <h2> The best place to bargain! </h2>
-      {loginState ? (
-        isEmpty(userObj) ? (
-          <div>Currently not Logged In</div>
-        ) : (
-          <div> Hello and Welcome {`${userObj.username}`}</div>
-        )
-      ) : null}
+      {userObj != null ? <div> Hello and Welcome {`${userObj.username}`}</div> : ( <div>Currently not Logged In</div>)}
+        
     </>
   );
 };
