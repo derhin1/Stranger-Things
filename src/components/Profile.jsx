33cac98b0a2019475be334d27b7aa62be074bc60
Alejandro <alexsagarnaga@gmail.com>
2022-03-18 17:27:12 -0500

    Completed styling of Profile Component including messages.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,65 +1,73 @@
-import React from 'react'
+import React from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = ({userObj}) => {
-    const history = useHistory();
+const Profile = ({ userObj }) => {
+  const history = useHistory();
 
-    function inboxDisplay(element){
-        return (
-          <div>
-            <div>From: {`${element.fromUser.username}`}</div>
-            <div>{`${element.content}`}</div>
-            <a onClick={() => {
-                handleClick(element.post._id)
-            }}>View My Post: {`${element.post.title}`}</a>
-          </div>
-        );
-    }
+  function inboxDisplay(element) {
+    return (
+      <div id="InsideMsg">
+        <div>From: {`${element.fromUser.username}`}</div>
+        <div>{`${element.content}`}</div>
+        <a
+          id="New"
+          onClick={() => {
+            handleClick(element.post._id);
+          }}
+        >
+          View My Post: {`${element.post.title}`}
+        </a>
+      </div>
+    );
+  }
 
-    function handleClick(postId){
-        history.push(`/Posts/${postId}`)
-    }
+  function handleClick(postId) {
+    history.push(`/Posts/${postId}`);
+  }
 
-    function myMessagesDisplay(element){
-        return (
-          <div>
-            <div>(Sent By Me)</div>
-            <div>{`${element.content}`}</div>
-            <a
-              onClick={() => {
-                handleClick(element.post._id);
-              }}
-            >
-              Message Again: {`${element.post.title}`}
-            </a>
-          </div>
-        );
-    }
+  function myMessagesDisplay(element) {
+    return (
+      <div id="InsideMsg">
+        <div>(Sent By Me)</div>
+        <div>{`${element.content}`}</div>
+        <a
+          id="New"
+          onClick={() => {
+            handleClick(element.post._id);
+          }}
+        >
+          Message Again: {`${element.post.title}`}
+        </a>
+      </div>
+    );
+  }
 
-
-    function messages(userObj){
-        return (
-            <>
-            <div>Welcome {`${userObj.username}`}</div>
-
-        <h3>Message Inbox</h3>
-        {userObj.messages.map((element) => {
-          return userObj.username != element.fromUser.username ? inboxDisplay(element) : null;
-        })}
-
-        <h3>Messages Sent</h3>
-        {userObj.messages.map((element) => {
-          return userObj.username === element.fromUser.username ? myMessagesDisplay(element) : null;
-        })}
-        </>
-        )
-    }
-
-    return ( 
+  function messages(userObj) {
+    return (
       <>
-      {userObj.username != null ? messages(userObj) : null}
+        <div id="Profile">
+          Welcome::<span id="User">{`${userObj.username}`}</span>!
+        </div>
+        <div id="Display">
+          <h3 id="Msg">Message Inbox</h3>
+          {userObj.messages.map((element) => {
+            return userObj.username != element.fromUser.username
+              ? inboxDisplay(element)
+              : null;
+          })}
+
+          <h3 id="Msg">Messages Sent</h3>
+          {userObj.messages.map((element) => {
+            return userObj.username === element.fromUser.username
+              ? myMessagesDisplay(element)
+              : null;
+          })}
+        </div>
       </>
     );
-}
+  }
 
-export default Profile
+  return <>{userObj.username != null ? messages(userObj) : null}</>;
+};
+
+export default Profile;
